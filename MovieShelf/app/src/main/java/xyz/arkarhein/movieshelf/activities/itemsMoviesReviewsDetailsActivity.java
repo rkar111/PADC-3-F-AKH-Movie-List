f73607b1b7189f391b797b9951e6ab3719916d13
Arkar Hein <rkarheinhein1@gmail.com>
2017-12-16 14:57:25 +0630

    Small Changes

--- MovieShelf/app/src/main/java/xyz/arkarhein/movieshelf/activities/itemsMoviesReviewsDetailsActivity.java
+++ MovieShelf/app/src/main/java/xyz/arkarhein/movieshelf/activities/itemsMoviesReviewsDetailsActivity.java
@@ -2,11 +2,14 @@ package xyz.arkarhein.movieshelf.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Window;
+import android.view.WindowManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +30,10 @@ public class itemsMoviesReviewsDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
         setContentView(R.layout.items_movies_details);
         ButterKnife.bind(this, this);
 
